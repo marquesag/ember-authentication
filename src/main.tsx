c9de205e9c22257fe69ac20c096406488c0bf588
Lucas Marques <marqueslucasag@gmail.com>
2023-12-12 19:20:12 -0300

    speed insights add

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Routing from './Routing'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Routing/>
+    <SpeedInsights/>
   </React.StrictMode>,
 )
