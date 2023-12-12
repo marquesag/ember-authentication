f17a022de6c493795373d68dbad8a1c18daebecd
Lucas Marques <marqueslucasag@gmail.com>
2023-12-12 19:03:04 -0300

    lol it's work

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from './routes/SignUp.tsx'
 import SignIn from './routes/SignIn.tsx'
+import Notfound from './routes/404/NotFound.tsx'
 
 export default function Routing(){
     return (
         <BrowserRouter>
         <Routes>
-            <Route path="*" element={<SignUp/>} />
+            <Route path="/" element={<SignUp/>} />
             <Route path="/sign-in" element={<SignIn/>} />
+            <Route path="*" element={<Notfound/>} />
         </Routes>
         </BrowserRouter>
     )
